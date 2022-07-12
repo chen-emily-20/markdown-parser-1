af35a4536c038f6553d96a2bc223d7783c53a06a
Emily Chen <emily.764537@gmail.com>
2022-07-12 08:32:58 -0700

    Merged the MarkdwonParseTest.java
    merged partner's test cases into one.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,12 +1,11 @@
 import static org.junit.Assert.*;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
+import java.util.*;
 import org.junit.*;
+
 public class MarkdownParseTest {
 
     @Test
@@ -21,6 +20,34 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(content);
         String[] expected = {};
         assertArrayEquals(expected, links.toArray());
+    }
+
+    @Test
+    public void testGetLinks1(){
+        try{
+            assertEquals(MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))), List.of("https://something.com","some-thing.html"));
+        }catch(Exception e){} 
+    }
+
+    @Test
+    public void testGetLinks2(){
+        try{
+            assertEquals(MarkdownParse.getLinks(Files.readString(Path.of("test-file2.md"))), List.of("https://google.com","some-thing.html"));
+        }catch(Exception e){} 
+    }
+
+    @Test
+    public void testGetLinks3(){
+        try{
+            assertEquals(MarkdownParse.getLinks(Files.readString(Path.of("test-file3.md"))), List.of("more text here"));
+        }catch(Exception e){} 
+    }
+
+    @Test
+    public void testGetLinks4(){
+        try{
+            assertEquals(MarkdownParse.getLinks(Files.readString(Path.of("test-file4.md"))), List.of());
+        }catch(Exception e){} 
     }
 
 }
